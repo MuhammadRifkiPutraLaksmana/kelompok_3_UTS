3d1f0578ddbd22efdd105917b65d08cfcb88830c
MuhammadRifkiPutraLaksmana <muhammadrifkiputralasmana@gmail.com>
2024-04-20 15:53:23 +0800

    Menambahkan tampilan untuk daftar menu makanan

--- lib/menu_makanan.dart
+++ lib/menu_makanan.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 
+
 class MenuMakanan extends StatefulWidget {
   @override
   _MenuMakananState createState() => _MenuMakananState();
@@ -113,6 +114,25 @@ class _MenuMakananState extends State<MenuMakanan>
                     'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQpdqJghJ2Wdra3-3aQqtfVU1UsAZjLs37A61cJ6eCHYcpl2y7FRrfqr1iJES0i',
                     'Rp 30.000',
                   ),
+                  SizedBox(height: 16.0),
+                  Text(
+                    'Minuman',
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.orange,
+                    ),
+                  ),
+                  _buildMenuCard(
+                    'Es Teh Manis',
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTUHc3j_KqCywhibwfmnJ4feUcdZBeROKy50-3uhLahQw8RpmWY5Hx804CcjOw',
+                    'Rp 10.000',
+                  ),
+                  _buildMenuCard(
+                    'Jus Alpukat',
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1bGLIaIUe_4XTOJO003rGzTAKaVjUySn_YA&usqp=CAU',
+                    'Rp 15.000',
+                  ),
                 ],
               ),
             ),
@@ -121,4 +141,72 @@ class _MenuMakananState extends State<MenuMakanan>
       ),
     );
   }
-    }
+
+  Widget _buildMenuCard(String title, String imageUrl, String price) {
+    return Card(
+      elevation: 4.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            Image.network(
+              imageUrl,
+              width: 100.0,
+              height: 100.0,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(width: 16.0),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Text(
+                    price,
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      _addToCart(title, imageUrl, price);
+                    },
+                    icon: Icon(Icons.add_shopping_cart),
+                    label: Text('Tambahkan'),
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.orange,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _addToCart(String title, String imageUrl, String price) {
+    setState(() {
+      _cartItems.add({
+        'title': title,
+        'imageUrl': imageUrl,
+        'price': price,
+      });
+    });
+    _saveCartItems();
+  }
+}
